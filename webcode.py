bf9b9f77d3043b77ca70f5e9e605c837b7759495
Shaijo George <shaijo.george@cofsosupport.in>
2025-06-28 16:16:26 +0530

    Implemented database connection using pymysql

--- webcode.py
+++ webcode.py
@@ -1,6 +1,16 @@
 from flask import *
+import pymysql
 
 app = Flask(__name__)
+app.secret_key = "qwe23"
+
+def get_db_connection():
+    return pymysql.connect(host='localhost', port=3306, user='root', passwd='', db='attendance_face')
+con = get_db_connection()
+
+@app.route('/')
+def index():
+    return "Welcome to Ezquela Attendance System"
 
 if __name__ == "__main__":
     app.run(debug=True)
